08b6fc9e49da73b3664660fc7757dba99e57116b
gufei <gufei@antaiib.com>
2021-12-23 17:12:56 +0800

    Initial commit

---
+++ spring-boot-project/spring-boot-cli/target/homebrew/springboot.rb
@@ -0,0 +1,27 @@
+require 'formula'
+
+class Springboot < Formula
+  homepage 'https://spring.io/projects/spring-boot'
+  url 'https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.1.3.RELEASE/spring-boot-cli-2.1.3.RELEASE-bin.tar.gz'
+  version '2.1.3.RELEASE'
+  sha256 '3d421baa740cf83d0376e6a186241c2aa3ec1ccd0aff4cf7e1d23a3b6b927644'
+  head 'https://github.com/spring-projects/spring-boot.git'
+
+  if build.head?
+    depends_on 'maven' => :build
+  end
+
+  def install
+    if build.head?
+      Dir.chdir('spring-boot-cli') { system 'mvn -U -DskipTests=true package' }
+      root = 'spring-boot-cli/target/spring-boot-cli-*-bin/spring-*'
+    else
+      root = '.'
+    end
+
+    bin.install Dir["#{root}/bin/spring"]
+    lib.install Dir["#{root}/lib/spring-boot-cli-*.jar"]
+    bash_completion.install Dir["#{root}/shell-completion/bash/spring"]
+    zsh_completion.install Dir["#{root}/shell-completion/zsh/_spring"]
+  end
+end
---
+++ study-source/src/main/java/test01/Test01.java
@@ -0,0 +1,21 @@
+package test01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.Assert;
+
+/**
+ * @description:
+ * @author:guf
+ * @createTime:2021/10/19 18:08
+ */
+@SpringBootApplication
+public class Test01 {
+	public static void main(String[] args) {
+		SpringApplication.run(Test01.class, args);
+//		System.getProperties()
+	}
+	public static void main(int a) {
+
+	}
+}
